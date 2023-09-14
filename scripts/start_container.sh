43b5e6b854e7ff2fe2f0372c68788eaa45392b64
Akula Jyothika <102134029+Bts-07@users.noreply.github.com>
2023-09-14 15:36:43 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull jyothika07/simple-python-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 jyothika07/simple-python-app
+docker run -d -p 5000:5000 jyothika07/simple-python-app 
